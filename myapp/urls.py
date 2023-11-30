bd1f8be11db97c8b0aa1b534226db37fa5475f32
Ubuntu <kliang622@gmail.com>
2023-11-30 01:16:04 +0000

    search page

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 from . import views
+from .views import hotSong
+
 urlpatterns = [
     path('', views.home, name='home'), 
-    # path('', views.season, name='season'),
+    path('myapp/hotSong/<int:year>/', views.hotSong, name='hotSong'), 
+    path('myapp/', views.season, name='season'),
+    # path('hotSong/<int:year>/', hotSong, name='hotSong'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -9,36 +9,35 @@ from django.core.serializers import serialize
 def home(request):
     return render(request, 'home.html')
 
-# def season(request):
-#      seasons = Season.objects.all()
-#      return render(request, 'seasons.html', {'seasons': seasons})
+def season(request):
+    # #  seasons = Season.objects.all()
+    #  return render(request, 'seasons.html')
 
-        # db_connection = mysql.connector.connect(user="hpham", password="halpal")
-        # db_cursor = db_connection.cursor()
-        # db_cursor.execute("USE cs179g;")
+        db_connection = mysql.connector.connect(user="hpham", password="halpal")
+        db_cursor = db_connection.cursor()
+        db_cursor.execute("USE cs179g;")
 
-        # return render(request, 'season.html')
 
         #fall
-        # db_cursor.execute("SELECT * FROM Seasons WHERE season='fall';")
-        # fall_data = db_cursor.fetchall()
+        db_cursor.execute("SELECT * FROM Seasons WHERE season='fall';")
+        fall_data = db_cursor.fetchall()
 
-        # #winter
-        # db_cursor.execute("SELECT * FROM Seasons WHERE season='winter';")
-        # winter_data = db_cursor.fetchall()
+        #winter
+        db_cursor.execute("SELECT * FROM Seasons WHERE season='winter';")
+        winter_data = db_cursor.fetchall()
 
-        # #spring
-        # db_cursor.execute("SELECT * FROM Seasons WHERE season='spring';")
-        # spring_data = db_cursor.fetchall()
+        #spring
+        db_cursor.execute("SELECT * FROM Seasons WHERE season='spring';")
+        spring_data = db_cursor.fetchall()
 
-        # #summer
-        # db_cursor.execute("SELECT * FROM Seasons WHERE season='summer';")
-        # summer_data = db_cursor.fetchall()
+        #summer
+        db_cursor.execute("SELECT * FROM Seasons WHERE season='summer';")
+        summer_data = db_cursor.fetchall()
 
-        # return render(request, 'seasons_template.html', {'spring_data': spring_data,
-        #                                                     'summer_data': summer_data,
-        #                                                     'autumn_data': fall_data,
-        #                                                     'winter_data': winter_data})
+        return render(request, 'season.html', {'spring_data': spring_data,
+                                                            'summer_data': summer_data,
+                                                            'autumn_data': fall_data,
+                                                            'winter_data': winter_data})
     # except Exception as e:
     #     return HttpResponse(f"An error occurred: {str(e)}")
     # finally:
@@ -46,3 +45,14 @@ def home(request):
     #     if db_connection.is_connected():
     #         db_cursor.close()
     #         db_connection.close()
+
+def hotSong(request, year):
+    db_connection = mysql.connector.connect(user="hpham", password="halpal")
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("USE cs179g;")
+
+    query = "SELECT field_name1, field_name2 FROM HottestSongsPerYear WHERE year = %s;"
+    db_cursor.execute(query, (year,))
+    data_for_year = db_cursor.fetchall()
+
+    return render(request, 'hotSong.html', {'year': year, 'data_for_year': data_for_year})
